8e74e22216019b8cc7cba4e6ce00ffe51514212f
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-19 23:09:46 +0530

    date_add function

--- practice/assignment2.sql
+++ practice/assignment2.sql
@@ -1,3 +1,12 @@
 show databases;
 use assg2;
 select ename, hiredate, sal, date_add(hiredate, interval 1 year) as salary_review from emp;
+select ename as Employee,
+	case 
+		when sal >1500 then sal
+        when sal=1500 then 'on target'
+        else 'below 1500'
+	end as salary_status
+from emp;
+    
+	

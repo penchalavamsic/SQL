01d1bc652ca071d684f551dcc4a98edfa88ee0b7
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-22 11:10:27 +0530

    drop & update procedures

---
+++ practice/Stored_Procedures.sql
@@ -0,0 +1,42 @@
+create database storedProcedures;
+use storedProcedures;
+
+-- schema code
+CREATE TABLE employees (
+emp_id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50),
+salary DECIMAL(10,2),
+department VARCHAR(30)
+);
+CREATE TABLE departments (
+dept_id INT AUTO_INCREMENT PRIMARY KEY,
+dept_name VARCHAR(30),
+location VARCHAR(30)
+);
+INSERT INTO employees (name, salary, department)
+VALUES ('Alice', 50000, 'HR'),
+('Bob', 60000, 'IT'),
+('Charlie', 70000, 'Finance');
+INSERT INTO departments (dept_name, location)
+VALUES ('HR', 'Hyderabad'),
+('IT', 'Bangalore'),
+('Finance', 'Delhi');
+
+-- deleting procedure
+DROP PROCEDURE IF EXISTS resetSal;
+
+
+-- operations
+-- Create a procedure to reset all employee salaries to 50000
+
+
+
+delimiter $$
+create procedure resetSal()
+begin
+	SET SQL_SAFE_UPDATES = 0; -- disabling safe updates
+ update employees set salary=50000;
+ select name , salary from employees;
+ end $$
+delimiter ;
+call resetSal();

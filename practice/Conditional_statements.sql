265f869e2b3653512b31a19943bacab41312edfd
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-22 21:31:38 +0530

    if-else - condition statement

--- practice/Conditional_statements.sql
+++ practice/Conditional_statements.sql
@@ -13,3 +13,17 @@ end if;
 end //
 delimiter ;
 call age(23);
+
+-- if else
+delimiter //
+create procedure quality(in a int)
+begin
+	if a>75
+		then
+			select 'good quality';
+	else
+		select 'bad quality';
+end if;
+end //
+delimiter ;
+call quality(76);

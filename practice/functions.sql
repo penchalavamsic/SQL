5c9397f6314d8686ac214cba1530b8bf95d8ad7a
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-23 22:29:51 +0530

    stored functions

--- practice/functions.sql
+++ practice/functions.sql
@@ -8,3 +8,14 @@ end //
 delimiter ;
 select addition(5,8);
 
+-- square of number
+delimiter //
+create function sq(n int)
+returns int deterministic
+begin
+	return n*n;
+end //
+delimiter ;
+select sq(10);
+    
+    
